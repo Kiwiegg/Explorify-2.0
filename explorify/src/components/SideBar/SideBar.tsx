221d3d4cda598207bb5be630dab1c1681b6d22f3
Larry <larrywutoronto@gmail.com>
2023-04-02 20:53:59 -0400

    Add tailwindCSS

---
+++ explorify/src/components/SideBar/SideBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './SideBar.css'
+import '../../styles/tailwind.css'
+
+const SideBar = () => {
+  return (
+    <div>SideBar</div>
+  )
+}
+
+export default SideBar
